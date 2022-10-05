3429c2d986152fc3a2a43104b4122f7e08bfee2f
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-10-04 21:58:36 -0800

    create add days function and display it

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let PSADate = document.querySelector('.PSADate').value
 let displayDate = document.querySelector('.date')
+let newDate = new Date()
+
+Date.prototype.addDays = function (days) {
+    const date = new Date(this.valueOf())
+    date.setDate(date.getDate() + days)
+    return date
+}
 
 function calculateDates(date) {
     PSADate = date
-    displayDate.textContent = `${PSADate}`
+    newDate = newDate.addDays(4)
+    displayDate.textContent = `${newDate.toISOString().split('T')[0]}`
 }
+
